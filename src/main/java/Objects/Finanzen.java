373985be92ec501686307c146094f1baec86232c
mschuhb <user@michischuhboeck.de>
2021-04-09 11:34:24 +0200

    Finanzen Update

--- src/main/java/Objects/Finanzen.java
+++ src/main/java/Objects/Finanzen.java
@@ -1,4 +1,15 @@
 package Objects;
 
 public class Finanzen {
+
+    double kontoguthaben;
+    char waehrung;
+
+    public void setWaehrung(char waehrung){
+        this.waehrung = waehrung;
+    }
+
+    public void getkontoguthaben(double kontoguthaben){
+        this.kontoguthaben = kontoguthaben;
+    }
 }
