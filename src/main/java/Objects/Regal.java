707c6c18c4105afe080c87d432e395ad1b4ff755
SebRod94 <basti.rodriguez@gmail.com>
2021-04-12 00:33:28 +0200

    Klasse Regal rudimentaer befüllt (für eine ID / Regal)

--- src/main/java/Objects/Regal.java
+++ src/main/java/Objects/Regal.java
@@ -1,5 +1,29 @@
 package Objects;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class Regal {
+    /* Eine Produkt-ID pro Regal:*/
+    private double kapazitaet;
+    private int containedID;
+    private int stueckzahl;
+
+    public Regal (double kapazitaet)
+    {
+    this.kapazitaet = kapazitaet;
+    }
+
+    public void setKapazitaet(double kapazitaet) { this.kapazitaet = kapazitaet; }
+
+    public double getKapazitaet() { return kapazitaet; }
+
+    public void setContainedID(int containedID) { this.containedID = containedID; }
+
+    public int getContainedID() { return containedID; }
+
+    public void setStueckzahl(int stueckzahl) { this.stueckzahl = stueckzahl; }
+
+    public int getStueckzahl() { return stueckzahl; }
 
 }
