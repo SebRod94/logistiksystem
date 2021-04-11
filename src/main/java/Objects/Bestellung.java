95e3b2c612ee2c7830c1694aa95cb435d6ae70f9
SebRod94 <basti.rodriguez@gmail.com>
2021-04-11 20:28:43 +0200

    Klasse Bestellung um Attribute und entsprechende Methoden erweitert

--- src/main/java/Objects/Bestellung.java
+++ src/main/java/Objects/Bestellung.java
@@ -1,7 +1,29 @@
 package Objects;
 
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
 public class Bestellung {
-    public void Order (int[] id){
+    static private int orderCnt = 1;
+    private int bestNr;
+    private String orderTime;
+    private int[] orderedIDs;
+    private boolean stornoStatus = false;
+
+    public Bestellung (int[] orderedIDs){
+        this.bestNr = orderCnt;
+        ++orderCnt;
+        this.orderTime = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss").format(new Date());
+        this.orderedIDs = orderedIDs;
+    }
+
+    public void storno () {
+        this.stornoStatus = true;
 
     }
+
+    public int getBestNr() { return bestNr; }
+    public String getOrderTime() { return orderTime; }
+    public int[] getOrderedIDs() { return orderedIDs; }
+    public boolean getStornoStatus() { return stornoStatus; }
 }
