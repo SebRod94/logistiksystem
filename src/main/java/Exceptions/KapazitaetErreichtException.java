acbc2ec74da3498aa7281af7eb73015a7a8c6cb1
Kilian Gallner <kilian.gallner@th-rosenheim.de>
2021-04-30 10:12:52 +0200

    added Exception

---
+++ src/main/java/Exceptions/KapazitaetErreichtException.java
@@ -0,0 +1,12 @@
+package Exceptions;
+
+public class KapazitaetErreichtException extends Exception{
+
+    public KapazitaetErreichtException(String ex)
+    {
+        super(ex);
+    }
+
+    public KapazitaetErreichtException(){
+    }
+}
