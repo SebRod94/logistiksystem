adfb6711af3e503e496a1c22d79e9d6d7bc5101f
MaxiThus <maximilian.thusbass@th-rosenheim.de>
2021-05-27 19:20:42 +0200

    .

--- src/main/java/Objects/Regal.java
+++ src/main/java/Objects/Regal.java
@@ -3,20 +3,46 @@ package Objects;
 import Lists.ArrList;
 
 import java.util.NoSuchElementException;
+import java.util.Scanner;
 
 public class Regal {
     private static int regalCnt = 1;
     private int id;
+    private Kategorie kategorie;
     private int kapazitaet;
     private int auslastung;
     private ArrList<Produkt> produkte;
 
 
-    public Regal (int kapazitaet, ArrList<Produkt> produkte) {
+    public Regal (int kapazitaet, ArrList<Produkt> produkte) throws Exception {
         this.kapazitaet = kapazitaet;
         this.produkte = produkte;
         this.id = regalCnt;
         regalCnt++;
+
+        System.out.printf("Zu welcher Kategorie gehört das Regal? Bitte Nummer eintippen:%n%n");
+        Kategorie[] kategorien = Kategorie.values();
+        for(Kategorie k : kategorien) {
+            System.out.println(k.ordinal() + " " + k);
+        }
+        Scanner scanner = new Scanner(System.in);
+        int index = Integer.parseInt(scanner.nextLine());
+        this.kategorie = kategorien[index];
+        scanner.close();
+        Lager[] lager = Lageruebersicht.getAlleLager();
+        ArrList<Sektor> sektoren = new ArrList<Sektor>();
+        for (int i = 0; i< lager.length;i++){
+            sektoren = lager[i].getSektoren();
+        }
+        Sektor[] alleSektoren = sektoren.toArray();
+        for (int i=0;i<alleSektoren.length;i++){
+            if (alleSektoren[i].getKategorie() == kategorie){
+                alleSektoren[i].addRegal(this);
+            }
+        }
+
+
+
     }
 
     public Regal (int kapazitaet) {
