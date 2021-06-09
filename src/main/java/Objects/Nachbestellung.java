85880f611de565745b4dff41762fc39eec73e4f4
SebRod94 <basti.rodriguez@gmail.com>
2021-06-09 17:29:39 +0200

    Typo Korrektur Klasse Nachbestellung

--- src/main/java/Objects/Nachbestellung.java
+++ src/main/java/Objects/Nachbestellung.java
@@ -6,18 +6,18 @@ import java.util.Map;
 
 public class Nachbestellung {
     static private int orderCnt = 1;
-    private int NachBestNr;
+    private int nachBestNr;
     private String orderTime;
     private Map<Integer, Integer> orderedIDs;
 
     public Nachbestellung (Map<Integer, Integer> orderedIDs){
-        this.NachBestNr = orderCnt;
+        this.nachBestNr = orderCnt;
         ++orderCnt;
         this.orderTime = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss").format(new Date());
         this.orderedIDs = orderedIDs;
     }
 
-    public int getNachBestNr() { return NachBestNr; }
+    public int getNachBestNr() { return nachBestNr; }
     public String getOrderTime() { return orderTime; }
     public Map<Integer, Integer> getOrderedIDs() { return orderedIDs; }
 }
