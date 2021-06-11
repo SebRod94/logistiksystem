ac569b3d06acbbf4e2f11c94a902653357b26f18
Kilian Gallner <kilian.gallner@th-rosenheim.de>
2021-06-11 08:25:41 +0200

    Reader Hinzugefügt (nicht schön aber funktionsfähig; überarbeitung folgt)

--- src/main/java/Datenverarbeitung/CSVReader.java
+++ src/main/java/Datenverarbeitung/CSVReader.java
@@ -3,15 +3,14 @@ package Datenverarbeitung;
 import Exceptions.KapazitaetErreichtException;
 import Exceptions.KeinRegalException;
 import Lists.ArrList;
-import Objects.Produkt;
+import Objects.*;
 
 import java.io.*;
 import java.util.Properties;
 
 public class CSVReader {
 
-    public ArrList<Produkt> readProdukte(){
-        String fileName = "G:\\Meine Ablage\\2. Semester\\OOP\\Test\\src\\ProjektMappe.csv";
+    public ArrList<Produkt> readProdukte(String fileName){
         File file = new File(fileName);
         String line = "";
         ArrList<Produkt> produkte = new ArrList<>();
@@ -36,11 +35,10 @@ public class CSVReader {
         return produkte;
     }
 
-    public ArrList<Produkt> readRegal(){
-        String fileName = "G:\\Meine Ablage\\2. Semester\\OOP\\Test\\src\\ProjektMappe.csv";
+    public ArrList<Regal> readRegal(String fileName){
         File file = new File(fileName);
         String line = "";
-        ArrList<Produkt> produkte = new ArrList<>();
+        ArrList<Regal> regale = new ArrList<>();
 
         try {
             FileReader inputStream = new FileReader(file);
@@ -48,17 +46,102 @@ public class CSVReader {
             br.readLine();
             while ((line = br.readLine()) != null) {
                 String[] values = line.split(";");
-                Produkt produkt = new Produkt(values[1], Integer.parseInt(values[2]), Double.parseDouble(values[3].replace(',','.')), Double.parseDouble(values[4].replace(',','.')), Integer.parseInt(values[5]));
-                produkte.add(produkt);
+                Regal regal = new Regal(Integer.parseInt(values[0].replace(',','.')));
+                regale.add(regal);
                 System.out.println(values[0]);
             }
             inputStream.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
-        } catch (IOException | KeinRegalException | KapazitaetErreichtException e) {
+        } catch (IOException | KapazitaetErreichtException e) {
             e.printStackTrace();
         }
 
-        return produkte;
+        return regale;
+    }
+
+    public ArrList<Sektor> readSektor(String fileName){
+        File file = new File(fileName);
+        String line = "";
+        ArrList<Sektor> sektors = new ArrList<>();
+
+        try {
+            FileReader inputStream = new FileReader(file);
+            BufferedReader br = new BufferedReader(inputStream);
+            br.readLine();
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(";");
+                Kategorie kategorie = getKategorie(removeUmlauts(values[1].trim().toLowerCase()));
+                Sektor sektor = new Sektor(Integer.parseInt(values[0].replace(',','.')), kategorie);
+                sektors.add(sektor);
+                System.out.println(values[0]);
+            }
+            inputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException | KapazitaetErreichtException e) {
+            e.printStackTrace();
+        }
+
+        return sektors;
+    }
+
+    public ArrList<Lager> readLager(String fileName) {
+        File file = new File(fileName);
+        String line = "";
+        ArrList<Lager> lagers = new ArrList<>();
+
+        try {
+            FileReader inputStream = new FileReader(file);
+            BufferedReader br = new BufferedReader(inputStream);
+            br.readLine();
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(";");
+                Lager lager = new Lager(Integer.parseInt(values[0].replace(',','.')), values[1], Integer.parseInt(values[2].replace(',','.')));
+                lagers.add(lager);
+                System.out.println(values[0]);
+            }
+            inputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return lagers;
+    }
+
+    private String removeUmlauts(String input)
+    {
+        input.replace("ä", "ae");
+        input.replace("ö", "oe");
+        input.replace("ü", "ue");
+        return input;
+    }
+
+    private Kategorie getKategorie(String kategorie)
+    {
+        switch (kategorie){
+            case "werkzeuge":
+                return Kategorie.Werkzeuge;
+            case "kleidung":
+                return Kategorie.Kleidung;
+            case "sportgeraete":
+                return Kategorie.Sportgeraete;
+            case "elektronik":
+                return Kategorie.Elektronik;
+            case "lebensmittel":
+                return Kategorie.Lebensmittel;
+            case "buerobedarf":
+                return Kategorie.Buerobedarf;
+            case "beleuchtung":
+                return Kategorie.Beleuchtung;
+            case "moebel":
+                return Kategorie.Moebel;
+            case "verpackungen":
+                return Kategorie.Verpackungen;
+        }
+
+        return null;
     }
 }
